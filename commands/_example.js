9f733875024f24cd97c5b97420711939de048787
1trickPonyta <1trickPonyta@kangaroostandard.com>
2020-08-24 20:56:45 -0700

    Make example command instructions more clear

--- commands/_example.js
+++ commands/_example.js
@@ -2,7 +2,9 @@
 	command.context: Twitch chat context
 	command.parameters: Command parameters
 	command.message: Entire message after the command without parsing parameters
-	command.respond(identity, message): Function that responds in chat as the labeled identity with a message
+	command.respond(identity, message):
+		identity: label used to identify the identity to respond as, for example "streamer"
+		message: message to respond with
 */
 
 module.exports = command => {
