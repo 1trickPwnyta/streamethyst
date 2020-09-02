bf95e59fd37099288b2ed84941b28bcb0784bb84
1trickPonyta <1trickPonyta@kangaroostandard.com>
2020-09-01 17:31:41 -0700

    Add admin override and verbose mode to cooldown feature

--- plugins/commands/features/cooldown.js
+++ plugins/commands/features/cooldown.js
@@ -1,4 +1,5 @@
 const log = require("../../../logger");
+const settings = require("../../../settings");
 
 module.exports = (options, plugin) => {
 	
@@ -7,13 +8,16 @@ module.exports = (options, plugin) => {
 	
 	let action = plugin.action;
 	plugin.action = context => {
-		if (!options.globalCooldownSeconds || 
-				!plugin.lastUsed || 
-				plugin.lastUsed < Date.now() - options.globalCooldownSeconds*1000) {
-					
-			if (!options.userCooldownSeconds || 
-					!plugin.userLastUsed[context.user["user-id"]] || 
-					plugin.userLastUsed[context.user["user-id"]] < Date.now() - options.userCooldownSeconds*1000) {
+		
+		let globalCooldownSecondsRemaining = Math.floor((!(options.adminOverride && context.user.admin) && 
+				options.globalCooldownSeconds? options.globalCooldownSeconds*1000 - (Date.now() - plugin.lastUsed): 0)/1000);
+		let userCooldownSecondsRemaining = Math.floor((!(options.adminOverride && context.user.admin) && 
+				options.userCooldownSeconds && plugin.userLastUsed[context.user["user-id"]]? 
+				options.userCooldownSeconds*1000 - (Date.now() - plugin.userLastUsed[context.user["user-id"]]): 0)/1000);
+		let cooldownSecondsRemaining = Math.max(globalCooldownSecondsRemaining, userCooldownSecondsRemaining);
+		
+		if (globalCooldownSecondsRemaining <= 0) {	
+			if (userCooldownSecondsRemaining <= 0) {
 				
 				plugin.lastUsed = Date.now();
 				plugin.userLastUsed[context.user["user-id"]] = Date.now();
@@ -28,9 +32,25 @@ module.exports = (options, plugin) => {
 			} else {
 				log.debug("Command skipped due to user cooldown.");
 			}
-			
 		} else {
 			log.debug("Command skipped due to global cooldown.");
+		}
+		
+		if (cooldownSecondsRemaining > 0 && options.verbose) {
+			let diff = cooldownSecondsRemaining;
+			
+			let hours = Math.floor(diff / (60 * 60));
+			diff -= hours * (60 * 60);
+
+			let mins = Math.floor(diff / 60);
+			diff -= mins * 60;
+
+			let seconds = Math.floor(diff);
+			diff -= seconds;
+			
+			context.chat("bot", `@${context.user["display-name"]}, please wait 
+					${("00" + hours).slice(-2)}:${("00" + mins).slice(-2)}:${("00" + seconds).slice(-2)}
+					to use ${settings.chatbot.commandPrefix}${context.command}.`);
 		}
 	};
 	
