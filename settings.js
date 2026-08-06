b80e6e74705001c29dc90ab3be2c1ae86b8cfe3f
1trickPwnyta <1trickPwnyta@kangaroostandard.com>
2026-08-05 21:59:08 -0700

    Add try catch for reading the settings file.

--- settings.js
+++ settings.js
@@ -1,3 +1,10 @@
 const { promises } = require("fs");
+const log = require("./logger");
 
-module.exports = async() => JSON.parse(await promises.readFile("./settings.json"));
+module.exports = async() => {
+	try {
+		return JSON.parse(await promises.readFile("./settings.json"));
+	} catch (e) {
+		log.error("Error while parsing settings file: " + e);
+	}
+};
